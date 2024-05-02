493bef628fbe53e3c56ed334a77c775e44251ad5
Rashad DuPaty <radupaty@gmail.com>
2024-05-02 19:51:46 +0900

    testing new nav

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -103,11 +103,19 @@ const navbar = document.querySelector(".navbar");
 
   // Ham-Burger Menu
 
-    const hamBurger=document.querySelector(".ham-burger");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-    hamBurger.addEventListener("click",function(){
-        document.querySelector(".navbar").classList.toggle("show");
-    })
+hamburger.addEventListener("click"), () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
 
 
 
